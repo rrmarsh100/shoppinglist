523f47ae349f77de2f0cb2a2bc88f68250eacba7
Robert Marsh <nhe6dc@gmail.com>
2018-07-09 09:16:02 -0400

    basic server

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Body Parser Middleware
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to mongo
+mongoose
+  .connect(db)
+  .then(() => console.log("mongoDB Connected..."))
+  .catch(err => console.log(err));
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${5000}`));
