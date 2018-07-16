fd186df5ca8379f78488f4ec0a9080ef0edb2618
Robert Marsh <nhe6dc@gmail.com>
2018-07-16 11:43:33 -0400

    keys

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://bobby:MauiMaui88@ds233061.mlab.com:33061/shoppinglist"
+};
--- server.js
+++ server.js
@@ -33,4 +33,4 @@ if (process.env.NODE_ENV === "production") {
 
 const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${5000}`));
+app.listen(port, () => console.log(`Server started on port ${port}`));
