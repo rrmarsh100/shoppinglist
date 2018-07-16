801ba4b1d8880d9d675e9efccb02115235b15604
Robert Marsh <nhe6dc@gmail.com>
2018-07-16 11:18:21 -0400

    deploy heroku

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const items = require("./routes/api/items");
 
@@ -20,6 +21,15 @@ mongoose
 
 // Use Routes
 app.use("/api/items", items);
+
+// Serve Static assets if in prod
+if (process.env.NODE_EVN === "production") {
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 const port = process.env.PORT || 5000;
 
